1b52ba93b826834b4076f12ab0a8aab2f38f4d4b
ppbreno <102385983+ppbreno@users.noreply.github.com>
2024-11-28 12:36:22 -0300

    Estruturação do projeto

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php include_once './partials/head.php'; ?>
+
+<body>
+  <section>
+    <?php include_once './partials/menu.php'; ?>
+    <?php include_once './pages/home.php'; ?>
+  </section>
+
+  <?php include_once './partials/scripts.php'; ?>
+</body>
+
+</html>
---
+++ admin/pages/home.php
@@ -0,0 +1,3 @@
+<div class="home_content">
+    <h1>Seja Bem-vindo</h1>
+</div>
